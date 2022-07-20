976c1ec54d7e2388bbb6b1f2bfd72a472dd267f1
callihiggins <calli@nyu.edu>
2022-07-19 20:52:48 -0400

    add routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { ThemeProvider } from 'styled-components';
+import ApolloClient  from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import theme from './theme'
 import './index.css';
 import Home from './components/Home';
+import Team from './components/Team';
+
+const client = new ApolloClient({
+  uri: `https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_SPACE_ID}?access_token=${process.env.REACT_APP_CONTENTFUL_TOKEN}`
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+  <div className="app">
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/team" element={<Team />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </ApolloProvider>
+  </div>
 );

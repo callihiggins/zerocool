09ad18a5ab60c4d99a4338b13659a1c8605a1a0c
callihiggins <calli@nyu.edu>
2022-07-20 17:17:05 -0400

    make jobs container flexible

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -5,6 +5,7 @@ import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 import Job from './Job';
 import * as styles from './styles.js'
+import { JobsContainer } from './styles.js';
 import * as sharedstyles from '../shared/styles.js';
 
 const JOBS_DATA = gql`
@@ -27,12 +28,19 @@ const Jobs = () => {
   return (
     <>
       <Nav active="jobs"></Nav>
-      <div css={styles.jobsSectionContainerClass}>
-        <div css={sharedstyles.titleBannerClass}>We just hired a few new team members, but check back later to join our growing team!</div>
-        <div css={styles.jobsContainerClass}>
-          {jobsData}
-        </div>
-      </div>
+      <JobsContainer hasJobs={jobsData?.length > 0}>
+        {jobsData?.length > 0 && (
+          <div css={sharedstyles.titleBannerClass}>We're hiring!</div>
+        )}
+        {jobsData?.length < 1 && (
+          <div css={styles.jobTitleClass}>We just hired a few new team members, but check back later to join our growing team!</div>
+        )}
+        {jobsData?.length > 0 && (
+          <div css={styles.jobsContainerClass}>
+            {jobsData}
+          </div>
+        )}
+      </JobsContainer>
       <Footer />
     </>
   )
--- src/components/Jobs/styles.js
+++ src/components/Jobs/styles.js
@@ -2,10 +2,20 @@ import { css } from '@emotion/react';
 import { default as theme } from '../../theme';
 import styled from 'styled-components';
 
-export const jobsSectionContainerClass = css`
+export const JobsContainer = styled.div`
   width: 100%;
   padding-top: 80px;
-
+  min-height: calc(100vh - 275px);
+  ${props => !props.hasJobs && `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `}
+  
+  @media (max-width: ${theme.breakpoints.large}px) {
+    padding-top: 0;
+    min-height: calc(100vh - 240px);
+  }
 `;
 
 export const jobsContainerClass = css`
@@ -25,6 +35,21 @@ export const jobsContainerClass = css`
 
   @media (max-width: ${theme.breakpoints.large}px) {
     justify-content: center;
+  }
+`;
+
+export const jobTitleClass = css`
+  text-align: center;
+  color: ${theme.colors.navy};
+  font-size: 36px;
+  letter-spacing: 1.5px;
+  font-weight: 400;
+  width: 60%;
+
+  @media (max-width: ${theme.breakpoints.large}px) {
+    top: 75px;
+    font-size: 28px;
+    width: 80%;
   }
 `;
 

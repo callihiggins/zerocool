408d022e57fae60886b724d3ff7c80c6ab465294
callihiggins <calli@nyu.edu>
2022-07-20 16:25:44 -0400

    move og up

--- src/Helmet.js
+++ src/Helmet.js
@@ -6,28 +6,18 @@ const PageHelmet = ({ keywords, title, image }) => {
   return (
     <>
       <Helmet title="ZeroCool Media">
-        <meta charset="utf-8" />
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <meta
-          name="description"
-          content="ZeroCool Media is a multidisciplinary production company"
-        />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-        <link rel="shortcut icon" type="image/x-icon" href="%PUBLIC_URL%/logo192.png"/>
-        <meta property="og:site_name" content="ZeroCool Media"/>
-        <meta property="og:title" content="ZeroCool Media"/>
-        <meta property="og:url" content="https://www.zerocool.tv"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:description" content="ZeroCool Media is a multidisciplinary production company"/>
         <meta property="og:image" content={SITE_IMAGE}/>
         <meta property="og:image:secure_url" content={SITE_IMAGE} />
         <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:alt" content="ZeroCool Logo" />
         <meta property="og:image:width" content="1080"/>
-        <meta property="og:image:height" content="1080"/>
+        <meta property="og:image:height" content="1080"/>>
+        <meta property="og:site_name" content="ZeroCool Media"/>
+        <meta property="og:title" content="ZeroCool Media"/>
+        <meta property="og:url" content="https://www.zerocool.tv"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:description" content="ZeroCool Media is a multidisciplinary production company"/>
+        
         <meta itemprop="name" content="ZeroCool Media"/>
         <meta itemprop="url" content="https://www.zerocool.tv"/>
         <meta itemprop="description" content="ZeroCool Media is a multidisciplinary production company"/>
